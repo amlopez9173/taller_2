db1095cb346a9360713c1ec5ac9b6f0edcc0d959
mauricio <lopez0315@hotmail.com>
2017-03-08 16:19:16 -0500

    falta corregir

---
+++ puntero_5/orden.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void orden(int *,int *);
+
+
+
+int main()
+{
+
+int n,*p=&n;
+int v[*p],*pv=v;
+
+printf("ingrese parametro del arreglo\n");
+scanf("%d",&n);
+
+for(int i=0;i<*p;i++)
+{
+    printf(" ingrese el numero %d :",i+1);
+    scanf("%d",&(*(pv+i)));
+}
+
+orden(pv,p);
+
+return 0;
+}
+
+
+void orden(int *pv,int *p)
+{
+    int aux1,aux2,c1;
+
+    for(int i=0;i<*p;i++)
+    {
+        aux1=*(pv+i);
+        for(int j=i;j<*p;j++)
+        {
+            if(aux1>=*(pv+j))
+            {
+               aux1=*(pv+j);
+               c1=j;
+            }
+        }
+        aux2=*(pv+i);
+        *(pv+i)=aux1;
+        *(pv+c1)=aux2;
+
+    }
+
+    for(int i=0;i<*p;i++)
+    {
+        printf("%d ",*(pv+i));
+
+    }
+}
